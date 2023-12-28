f37f67f37c8736370efbac67e85c2b3ed9a60ba3
tayrapolikar <tayra.polikar@post.idc.ac.il>
2023-12-28 16:58:06 +0200

    test 3

--- OneOfEach.java
+++ OneOfEach.java
@@ -3,7 +3,7 @@ public class OneOfEach {
 		boolean girl=false;
 		boolean boy=false;
 		int numbofchildren=0;
-		while (!(girl||boy)){
+		while (!(girl&&boy)){
 			double gender=Math.random();
 			if(gender<0.5){
 				System.out.println("b");
@@ -16,5 +16,3 @@ public class OneOfEach {
 			System.out.println("You made it... and now you have "+numbofchildren+" children.");
 
 		}
-	}
-}
